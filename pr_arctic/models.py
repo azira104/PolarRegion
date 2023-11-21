2f910662f94e4f1c581e1f62061acda1756e1b96
NurHaznirah <nurhaznirah@graduate.utm.my>
2023-11-21 16:44:49 +0800

    new updates

--- pr_arctic/models.py
+++ pr_arctic/models.py
@@ -13,7 +13,7 @@ class iceExtent(models.Model):
 class northernClimate(models.Model):
     id = models.AutoField(primary_key=True)
     city = models.CharField(max_length=100)
-    country= models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
     year = models.IntegerField()
@@ -22,4 +22,43 @@ class northernClimate(models.Model):
     east_west = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.city
+        return self.country
+    
+class alaskaStreamgages(models.Model):
+    id = models.AutoField(primary_key=True)
+    agency = models.CharField(max_length=10)
+    location_id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    lat_long_type = models.CharField(max_length=10)
+    country = models.CharField(max_length=255)
+    hydrologic_unit = models.IntegerField()
+    drainage_area = models.FloatField()
+    datum_of_page = models.FloatField()
+    datum_type = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+class simpsonLagoon(models.Model):
+    id = models.AutoField(primary_key=True)
+    volume = models.FloatField()
+    mid_sampling = models.DateTimeField()
+    season = models.CharField(max_length=100)
+    year = models.IntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    salinity = models.FloatField()
+    temperature = models.FloatField()
+    radium224 = models.FloatField()
+    error224 = models.FloatField()
+    radium223 = models.FloatField()
+    error223 = models.FloatField()
+    radium228 = models.FloatField()
+    error228 = models.FloatField()
+    radium226 = models.FloatField()
+    error226 = models.FloatField()
+
+    def __str__(self):
+        return self.season
--- pr_disease/models.py
+++ pr_disease/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+
+class birdInfluenza_bELISA(models.Model):
+    id = models.AutoField(primary_key=True)
+    lab_ID = models.CharField(max_length=50)
+    field_ID = models.CharField(max_length=100)
+    host_species = models.CharField(max_length=100)
+    age = models.CharField(max_length=50)
+    sampling_date = models.DateField()
+    location = models.CharField(max_length=255)
+    location_notes = models.TextField()
+    lat = models.FloatField()
+    long = models.FloatField()
+    test_date = models.DateField()
+    sample_ABS = models.FloatField()
+    neg_ctrl = models.FloatField()
+    S_N_ratio = models.FloatField()
+    result = models.CharField(max_length=10)
+    replicate = models.IntegerField()
+
+    def __str__(self):
+        return self.host_species
+    
+class birdInfluenza_HI(models.Model):
+    id = models.AutoField(primary_key=True)
+    lab_ID = models.CharField(max_length=50)
+    host_species = models.CharField(max_length=255)
+    H1N2 = models.CharField(max_length=10)
+    H2N2 = models.CharField(max_length=10)
+    H3N2 = models.CharField(max_length=10)
+    H4N2 = models.CharField(max_length=10)
+    H5N2 = models.CharField(max_length=10)
+    H6N2 = models.CharField(max_length=10)
+    H7N3 = models.CharField(max_length=10)
+    H8N4 = models.CharField(max_length=10)
+    H9N2 = models.CharField(max_length=10)
+    H10N2 = models.CharField(max_length=10)
+    H11N2 = models.CharField(max_length=10)
+    H12N2 = models.CharField(max_length=10)
+    H13N6 = models.CharField(max_length=10)
+    H14N2 = models.CharField(max_length=10)
+    H16N3 = models.CharField(max_length=10)
+    HI_result = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.host_species
+
+class wildlife_cysts_oocysts(models.Model):
+    id = models.AutoField(primary_key=True)
+    field_ID = models.CharField(max_length=20)
+    lab_ID = models.CharField(max_length=20)
+    species = models.CharField(max_length=100)
+    common_name = models.CharField(max_length=20)
+    date = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    age_class = models.CharField(max_length=10)
+    sex = models.CharField(max_length=10)
+    cryptosporidium = models.CharField(max_length=10)
+    giardia = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.common_name 
