8bc1ca7f1af10d390b274fd53961f89feb16126f
NurHaznirah <nurhaznirah@graduate.utm.my>
2023-11-22 16:18:43 +0800

    new updates

--- pr_disease/urls.py
+++ pr_disease/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-app_name = 'pr_arctic'
+app_name = 'pr_disease'
 
 urlpatterns = []
