0a12d748fe936aae6468206ffb3b92f134e6b0a9
zira <rohaizazira@gmail.com>
2023-11-26 09:44:34 +0800

    update size logo
    Update logo and contact form

--- pr_antartic/views.py
+++ pr_antartic/views.py
@@ -8,13 +8,5 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
-
-def inner_page(request):
-    return render(request, 'inner-page.html')
-
-def portfolio_details(request):
-    return render(request, 'portfolio-details.html')
-
-
+def antartic(request):
+    return render(request, 'antartic.html')
