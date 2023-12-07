1122bfc64b29ad00d613dd1456e6718a5046b6a0
NurHaznirah <nurhaznirah@graduate.utm.my>
2023-12-07 10:40:57 +0800

    commit

--- static/js/pr_aa.js
+++ static/js/pr_aa.js
@@ -1,32 +1,38 @@
 $(document).ready(function () {
-  var table = $("#sulfur_table").DataTable({
-    processing: true,
+
+  $('#sulfur_table').DataTable( {
     deferRender: true,
     scrollCollapse: true,
     fixedColumns: true,
     paging: true,
     responsive: true,
-    // fixedHeader: {
-    //   header: true,
-    //   footer: true,
-     
-    // },
-    
+    fixedHeader: {
+      header: true,
+      footer: true,
+    },
+
     dom: "Bfrtip",
-    buttons: ['csv', 'pdf'],
+    buttons: ["csv", "pdf"],
   });
-  $(".dataTables_length select, .dataTables_filter input, .dataTables_info, .dataTables_paginate").css('color', 'white');
+
+  // $(document).ready(function () {
+  //   $(
+  //     "div.dt-buttons>.dt-button, div.dt-buttons>div.dt-button-split .dt-button"
+  //   ).css({
+  //     color: "white",
+  //     // Add any other styles for these buttons as needed
+  //   });
+  //   $(
+  //     ".dataTables_paginate .paginate_button.disabled, .dataTables_paginate .paginate_button.disabled:hover, .dataTables_paginate .paginate_button.disabled:active"
+  //   ).css({
+  //     cursor: "default",
+  //     color: "white",
+  //     border: "1px solid white",
+  //     background: "white",
+  //     boxShadow: "none",
+  //   });
+  //   $(
+  //     ".dataTables_length select, .dataTables_filter input, .dataTables_info, .dataTables_paginate"
+  //   ).css("color", "white");
+  // });
 });
-$(".dataTables_paginate .paginate_button.disabled, .dataTables_paginate .paginate_button.disabled:hover, .dataTables_paginate .paginate_button.disabled:active")
-    .css({
-      cursor: 'default',
-      color: 'white',
-      border: '1px solid white',
-      background: 'white',
-      boxShadow: 'none'
-    });
-    $("div.dt-buttons>.dt-button, div.dt-buttons>div.dt-button-split .dt-button")
-    .css({
-      color: 'white'
-      // Add any other styles for these buttons as needed
-    });
