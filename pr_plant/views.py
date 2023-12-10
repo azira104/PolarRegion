ecec120762c1c033b6dfc1c0919ff999eb3f9810
NurHaznirah <nurhaznirah@graduate.utm.my>
2023-12-10 17:02:53 +0800

    commit

--- pr_plant/views.py
+++ pr_plant/views.py
@@ -6,4 +6,14 @@ from django.shortcuts import render, redirect, get_object_or_404, resolve_url
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+# calling from models.py
+from .models import microbialSymbionts, glacierAlgae
+
 # Create your views here.
+def microbial(request):
+    microbial_data = microbialSymbionts.objects.all()
+    return render(request, 'microbial.html', {'microbial': microbial_data})
+
+def algae(request):
+    algae_data = glacierAlgae.objects.all()
+    return render(request, 'algae.html', {'algae': algae_data})
--- static/js/pr_aa.js
+++ static/js/pr_aa.js
@@ -17,25 +17,4 @@ $(document).ready(function () {
     dom: "Bfrtip",
     buttons: ["csv", "pdf"],
   });
-
-  // $(document).ready(function () {
-  //   $(
-  //     "div.dt-buttons>.dt-button, div.dt-buttons>div.dt-button-split .dt-button"
-  //   ).css({
-  //     color: "white",
-  //     // Add any other styles for these buttons as needed
-  //   });
-  //   $(
-  //     ".dataTables_paginate .paginate_button.disabled, .dataTables_paginate .paginate_button.disabled:hover, .dataTables_paginate .paginate_button.disabled:active"
-  //   ).css({
-  //     cursor: "default",
-  //     color: "white",
-  //     border: "1px solid white",
-  //     background: "white",
-  //     boxShadow: "none",
-  //   });
-  //   $(
-  //     ".dataTables_length select, .dataTables_filter input, .dataTables_info, .dataTables_paginate"
-  //   ).css("color", "white");
-  // });
 });
