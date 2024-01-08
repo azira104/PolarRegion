ccc2d7f75fed84cf14f2501a0685d96cd11601a5
zira <rohaizazira@gmail.com>
2024-01-08 09:16:33 +0800

    update team page
    already update 7/9 team members profile

--- mainpr/views.py
+++ mainpr/views.py
@@ -11,6 +11,22 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'index.html')
 
+def drakmal(request):
+    return render(request, 'drakmal.html')
+
+def drmae(request):
+    return render(request, 'drmae.html')
+def dralfonso(request):
+    return render(request, 'dralfonso.html')
+def drismail(request):
+    return render(request, 'drismail.html')
+def drchin(request):
+    return render(request, 'drchin.html')
+def profsaberi(request):
+    return render(request, 'profsaberi.html')
+def drsarahani(request):
+    return render(request, 'drsarahani.html')
+
 def inner_page(request):
     return render(request, 'inner-page.html')
 
