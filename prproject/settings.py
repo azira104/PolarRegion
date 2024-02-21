4ce72f49b43694379a23b5f71dca0a928e7d6c6e
tuaniqram <tuaniqram0@gmail.com>
2024-02-21 10:22:18 +0800

    Update settings.py

--- prproject/settings.py
+++ prproject/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'prproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'polarregion',
-        'USER': 'root',
-        'PASSWORD': '',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST':'localhost',
         'PORT':'3306',
     }
