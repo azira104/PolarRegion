726573f92931e5edf2960d62027ceb71393bbd0c
Tuaniqram <tuaniqram0@gmail.com>
2024-12-12 11:55:26 +0800

    Update settings.py

--- prproject/settings.py
+++ prproject/settings.py
@@ -118,8 +118,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tuaniqram0@gmail.com'
-EMAIL_HOST_PASSWORD = 'hpqreyachruonhye'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
